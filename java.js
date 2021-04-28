9888c78f7445dad8c047dd60a43729fa97e1d0e5
aden <aconrod1@gmail.com>
2021-04-28 13:31:37 -0600

    commit

--- java.js
+++ java.js
@@ -1,8 +1,34 @@
-var bro = document.getElementById("howdy").value;
-function test(){
-    if (bro === true){
-    alert("hay");
-}else{
-    alert("howdy")
+var imageresponce;
+var textResponce;
+var factVerificationStatus;
+ 
+async function buttonFunction() {
+    await getcatimage()
+    await getCatFactTrue()
+    while (true) {
+        if (factVerificationStatus === true) {
+            break;
+        } else {
+            await getCatFactTrue()
+        }
+    }
+ 
 }
+ 
+ 
+async function getcatimage() {
+    var response = await fetch('https://cataas.com/cat?json=true')
+    var data1 = await response.json()
+    document.getElementById("apiResponceImage").src = "https://cataas.com" + data1.url;
+ 
+}
+ 
+async function getCatFactTrue() {
+    var response = await fetch("https://cat-fact.herokuapp.com/facts/random")
+    data = await response.json()
+    console.log(data.status.verified)
+    factVerificationStatus = data.status.verified
+    console.log(data.text)
+    textResponce = data.text
+    document.getElementById("textToBechanged").innerHTML = textResponce;
 }
